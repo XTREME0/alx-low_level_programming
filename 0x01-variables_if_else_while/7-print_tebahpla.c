2abfef4415f570783b1ec0662abde1689d017256
Anas <taaianas.officiel@gmail.com>
2023-08-11 15:16:23 -0700

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char a[] = "zyxwvutsrqponmlkjihgfedcba\n";
+
+	while (a[i] != '\0')
+	{
+		putchar(a[i]);
+		i += 1;
+	}
+	return (0);
+}
