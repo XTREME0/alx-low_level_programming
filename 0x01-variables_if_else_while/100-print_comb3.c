2443573a6788b65c90db902ad1dc13b7dbc90e4c
Anas <taaianas.officiel@gmail.com>
2023-08-14 06:41:00 -0700

    added 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 9)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+	}
+	putchar('\n');
+	return (0);
+}
