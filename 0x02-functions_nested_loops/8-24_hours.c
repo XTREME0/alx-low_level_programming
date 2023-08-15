7522d1cac3fbcf45e14b301f2065a0e05e47482b
Anas <taaianas.officiel@gmail.com>
2023-08-15 11:47:09 -0700

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * jack_bauer - print from 00:00 to 23:59
+ *
+ * Return: time
+ *
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	while (i <= 2)
+	{
+		while (j <= 3)
+		{
+			while (k <= 5)
+			{
+				while (l <= 9)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(":");
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar("\n");
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+
+}
