258d1f7106f823dc919b914f02e60ebde3497a2a
Anas <taaianas.officiel@gmail.com>
2023-08-15 15:51:50 -0700

    added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from n to 98
+ *
+ *@c: is the start
+ * Return: nothing
+ *
+ */
+void print_to_98(int c)
+{
+	while (c <= 98)
+	{
+		if ((c / 10) > 0)
+		{
+			_putchar('0' + (c / 10));
+			_putchar('0' + (c % 10));
+		} else
+		{
+			 _putchar('0' + c);
+		}
+		if (c != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		c++;
+	}
+
+	return (0);
+}
