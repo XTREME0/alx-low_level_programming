d837738a7fba0d29f0ff0101e87be38762c801aa
Anas <taaianas.officiel@gmail.com>
2023-08-15 11:28:07 -0700

    added 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,14 @@
  */
 int print_last_digit(int c)
 {
+	int last_digit;
+
 	if (c < 0)
 	{
 		c = -c;
 	}
-	int last_digit = c % 10;
 
+	last_digit = c % 10;
 	_putchar('0' + last_digit);
 	return (last_digit);
 }
