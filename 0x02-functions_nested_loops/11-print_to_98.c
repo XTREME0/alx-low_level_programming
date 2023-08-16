bfb8f5f2b60d3dff9a1322b6c7a6dddc2b0ef44d
Anas <taaianas.officiel@gmail.com>
2023-08-15 18:27:05 -0700

    added 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,65 +1,94 @@
 #include "main.h"
+
 /**
- * print_to_98 - prints from n to 98
+ * printit - writes the character c using _putchar
+ * @c: The character to print
  *
- *@c: is the start
+ * Return: On success 1.
+ *
+ */
+void printit(int c)
+{
+	if ((c / 100) > 0)
+	{
+		_putchar('0' + (c / 100));
+		_putchar('0' + ((c / 10) % 10));
+		_putchar('0' + (c % 10));
+	} else
+	{
+		if ((c / 10) > 0)
+		{
+			_putchar('0' + (c / 10));
+			_putchar('0' + (c % 10));
+		} else
+		{
+			 _putchar('0' + c);
+		}
+	}
+}
+/**
+ * ft - foot
+ *
+ * @c: The character to print
+ *
  * Return: nothing
+ */
+void ft(int c)
+{
+
+	if (c != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	} else
+	{
+		if (sw == 0)
+		{
+			_putchar('\n');
+		}
+	}
+	if (sw == 0)
+	{
+		c++;
+	} else
+	{
+		if (c == 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		c--;
+		c = -c;
+		sw = 0;
+	}
+}
+
+/**
+ * print_to_98 - writes numbers from n to 98
+ * @c: where to start
  *
+ * Return: nothing
  */
 void print_to_98(int c)
 {
 	int sw = 0;
 
-	while (c != 98)
+	while (c != 99)
 	{
+		while (c > 98)
+		{
+		printit(c);
+		_putchar(',');
+		_putchar(' ');
+		c--;
+		}
 		if (c < 0)
 		{
 			c = -c;
 			sw = 1;
 			_putchar('-');
 		}
-
-		if ((c / 100) > 0)
-		{
-			_putchar('0' + (c / 100));
-			_putchar('0' + ((c / 10) % 10));
-			_putchar('0' + (c % 10));
-
-		} else
-		{
-			if ((c / 10) > 0)
-			{
-				_putchar('0' + (c / 10));
-				_putchar('0' + (c % 10));
-			} else
-			{
-			 	_putchar('0' + c);
-			}
-		}
-		if (c != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		} else
-		{
-			if (sw == 0)
-			{
-				_putchar('\n');
-			}
-		}
-		if (sw == 0)
-		{
-			c++;
-		} else
-		{
-			if (c == 98)
-			{
-				_putchar(',');
-                        	_putchar(' ');
-			}
-			c--;
-			c = -c;
-			sw = 0;
-		}
+		printit(c);
+		ft(c);
 	}
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98.cc
@@ -0,0 +1,65 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from n to 98
+ *
+ *@c: is the start
+ * Return: nothing
+ *
+ */
+void print_to_98(int c)
+{
+	int sw = 0;
+
+	while (c != 98)
+	{
+		if (c < 0)
+		{
+			c = -c;
+			sw = 1;
+			_putchar('-');
+		}
+
+		if ((c / 100) > 0)
+		{
+			_putchar('0' + (c / 100));
+			_putchar('0' + ((c / 10) % 10));
+			_putchar('0' + (c % 10));
+
+		} else
+		{
+			if ((c / 10) > 0)
+			{
+				_putchar('0' + (c / 10));
+				_putchar('0' + (c % 10));
+			} else
+			{
+			 	_putchar('0' + c);
+			}
+		}
+		if (c != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		} else
+		{
+			if (sw == 0)
+			{
+				_putchar('\n');
+			}
+		}
+		if (sw == 0)
+		{
+			c++;
+		} else
+		{
+			if (c == 98)
+			{
+				_putchar(',');
+                        	_putchar(' ');
+			}
+			c--;
+			c = -c;
+			sw = 0;
+		}
+	}
+}
