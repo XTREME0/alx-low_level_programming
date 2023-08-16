4b96e4c447b63dd5a917a91bc7dad39d92f07278
Anas <taaianas.officiel@gmail.com>
2023-08-16 16:19:45 -0700

    added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUCCESS)
+ *
+ */
+int main(void)
+{
+	int counter = 0;
+	long x;
+	long y = 0;
+	long z = 1;
+
+	while (counter < 50)
+	{
+		x = y;
+		y = z;
+		z = x + y;
+		printf("%ld, ", z);
+		counter++;
+	}
+	printf("\n");
+	return (0);
+}
