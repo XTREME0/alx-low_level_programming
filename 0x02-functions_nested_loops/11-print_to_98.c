81fee54f63408e3bf19d7a68cb9970aff36ee7b0
XTREME <58626940+XTREME0@users.noreply.github.com>
2023-08-16 01:00:04 +0000

    added 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,19 @@ void printit(int c)
 {
     if ((c / 100) > 0)
 		{
-			putchar('0' + (c / 100));
-			putchar('0' + ((c / 10) % 10));
-			putchar('0' + (c % 10));
+			_putchar('0' + (c / 100));
+			_putchar('0' + ((c / 10) % 10));
+			_putchar('0' + (c % 10));
 
 		} else
 		{
 			if ((c / 10) > 0)
 			{
-				putchar('0' + (c / 10));
-				putchar('0' + (c % 10));
+				_putchar('0' + (c / 10));
+				_putchar('0' + (c % 10));
 			} else
 			{
-			 	putchar('0' + c);
+			 	_putchar('0' + c);
 			}
 		}
 }
@@ -43,8 +43,8 @@ void print_to_98(int c)
 	    while (c > 98)
 	    {
 	        printit(c);
-	        putchar(',');
-			putchar(' ');
+	        _putchar(',');
+		_putchar(' ');
 	        c--;
 	        
 	    }
@@ -52,19 +52,19 @@ void print_to_98(int c)
 		{
 			c = -c;
 			sw = 1;
-			putchar('-');
+			_putchar('-');
 		}
 
 		printit(c);
 		if (c != 98)
 		{
-			putchar(',');
-			putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 		} else
 		{
 			if (sw == 0)
 			{
-				putchar('\n');
+				_putchar('\n');
 			}
 		}
 		if (sw == 0)
@@ -74,8 +74,8 @@ void print_to_98(int c)
 		{
 			if (c == 98)
 			{
-				putchar(',');
-                putchar(' ');
+				_putchar(',');
+                		_putchar(' ');
 			}
 			c--;
 			c = -c;
