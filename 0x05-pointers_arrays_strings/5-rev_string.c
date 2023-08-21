38bd8dc4cbfd6582f086f9da58b493ac947591db
Anas <taaianas.officiel@gmail.com>
2023-08-21 11:10:48 -0700

    added 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -31,7 +31,8 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int len = _strlen(s) - 1;
-	char str[len + 1];
+	const int why_wouldnt_it_just_take_len_as_length = len + 1;
+	char str[why_wouldnt_it_just_take_len_as_length];
 
 	for (; len >= 0; len--, i++)
 	{
