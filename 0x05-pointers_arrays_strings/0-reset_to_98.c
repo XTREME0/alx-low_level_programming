17860cdd3b460619f8744bf36ff2c7483089558a
Anas <taaianas.officiel@gmail.com>
2023-08-21 05:34:23 -0700

    added README.md

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes value of int to 98
+ *
+ *@n: is the pointer to int value
+ * Return: 0 success
+ *
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this dir is for practicing pointers and arrays, strings
