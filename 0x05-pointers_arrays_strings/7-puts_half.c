15bb2b75240e9565a26db6feb8fab3c370ebfd1e
Anas <taaianas.officiel@gmail.com>
2023-08-21 13:53:14 -0700

    added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ *
+ *@s: is the first int
+ *
+ * Return: len of s
+ *
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * puts_half - prints half of a string
+ *
+ *@str: is the string
+ *
+ * Return: nothing
+ *
+ */
+void puts_half(char *str)
+{
+	int end = _strlen(s);
+	int start;
+
+	if ((end % 2) != 0)
+	{
+		end--;
+	}
+	for (start = end / 2; start < end; start++)
+	{
+		putchar(*(str + start));
+	}
+	printf("\n");
+}
