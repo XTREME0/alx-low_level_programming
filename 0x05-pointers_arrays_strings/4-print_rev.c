d4f19206dec6d1a6356ee1aa2ee45570eac9a9fe
Anas <taaianas.officiel@gmail.com>
2023-08-21 10:01:41 -0700

    added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ *
+ *@s: is the first int
+ *
+ * Return: len of s
+ *
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_rev - prints reverse of a string
+ *
+ *@s: is the string
+ *
+ * Return: nothing
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int len = _strlen(s) - 1;
+
+	for (; len >= 0; len--)
+	{
+		putchar(*(s + len));
+	}
+	printf("\n");
+}
