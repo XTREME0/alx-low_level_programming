e2776ad8688af011928a5f222b7e0131a14fb9fe
xtreme <anastaai94@gmail.com>
2023-08-29 13:52:03 +0100

    fixed 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * set_string - sets the value of a pointer to a char 
+ * set_string - sets the value of a pointer to a char
  *
  *@s: the pointer
  *@to: the char
@@ -10,7 +10,5 @@
  */
 void set_string(char **s, char *to)
 {
-	*s = (char *)malloc(strlen(to) + 1);
-
-	 strcpy(*s, to);
+	 *s = to;
 }
