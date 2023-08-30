0b99ae99498785b26223b5ddfe555aebdd6b911a
xtreme <anastaai94@gmail.com>
2023-08-30 16:16:49 +0100

    fixed 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,12 +14,10 @@ int factorial(int n)
 	if (n > 0)
 	{
 		return (n * factorial(n - 1));
-	} else
+	}
+	if (n == 0)
 	{
-		if (n == 0)
-		{
-			return (1);
-		}
+		return (1);
 	}
 	return (-1);
 }
