29579fa5d8ac6d4b90cd528cf50afdfcc86830d6
xtreme <anastaai94@gmail.com>
2023-08-30 14:08:34 +0100

    added 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,12 +10,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i = -1;
 
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		i++;
-		_print_rev_recursion(s + i);
-		_putchar(s[i]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
