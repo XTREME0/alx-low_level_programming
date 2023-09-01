3d5aa9ae9cf4ba23e3cdc66aa551579a8b1effaa
xtreme <anastaai94@gmail.com>
2023-09-01 23:32:27 +0100

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *@argc: number of args
+ *@argv: pointer to args
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
