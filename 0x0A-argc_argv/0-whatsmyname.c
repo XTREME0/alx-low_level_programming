dfbf6ad0d9c6155e0e09fdc975a115f883eb2c96
xtreme <anastaai94@gmail.com>
2023-09-01 22:19:58 +0100

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Return: 0 in case of success
+ */
+int main(int argc, char *argv[])
+{
+	int x = argc;
+	x++;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This dir is for argc and argv
