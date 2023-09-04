ff409ecf9201cb8ddd1170d939e0672d5e828588
xtreme <anastaai94@gmail.com>
2023-09-04 21:48:13 +0100

    fixed 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,15 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 		return (NULL);
-	char *array = malloc(sizeof(char) * size);
-	return (*array);
+	array = malloc(sizeof(char) * size);
+	for(i = 0; i < size; i++)
+	{
+		*(array + i) = c;
+	}
+	return (array);
 }
