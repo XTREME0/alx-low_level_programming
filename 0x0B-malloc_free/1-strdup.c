736dd9413946dadac038ea6413e8c39be2c7c0b8
xtreme <anastaai94@gmail.com>
2023-09-06 00:42:39 +0100

    fixed 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a str which is a duplicate of the string str
+ *
+ *@str: the string
+ * Return: pointer to a str which is a duplicate of the string str
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+
+	if (malloc(sizeof(str)) == NULL)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(str));
+	strcpy(array, str);
+	return (array);
+}
