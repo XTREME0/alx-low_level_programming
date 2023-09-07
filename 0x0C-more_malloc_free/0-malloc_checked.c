16c99f93091a85eb87d93d56b2b55f6147a43dd3
xtreme <anastaai94@gmail.com>
2023-09-07 19:22:16 +0100

    added 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Returns a pointer to the allocated memory
+ *
+ *@b: size
+ *Return: Returns a pointer to the allocated memory
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *array;
+
+	if (b == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(b);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	return (array);
+}
