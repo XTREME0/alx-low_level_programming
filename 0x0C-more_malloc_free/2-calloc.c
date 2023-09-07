b5ae78cff1f0446adb39d1af859e4a0834edf154
xtreme <anastaai94@gmail.com>
2023-09-07 23:43:42 +0100

    added 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,12 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
+	void *array;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc(nmemb * size);
 	if (array == NULL)
 	{
