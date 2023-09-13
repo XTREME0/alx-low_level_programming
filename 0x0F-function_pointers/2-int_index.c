60e14981f39e0ac5cb4469be6e356a35552e1c55
xtreme <anastaai94@gmail.com>
2023-09-14 00:35:50 +0100

    added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * int_index - search for int
+ *@array: the array
+ *@cmp: pointer to fonction
+ *@size: the size of array
+ * Return: the index
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if((*cmp)(array(i)))
+			return i;
+	}
+	return (-1);
+}
