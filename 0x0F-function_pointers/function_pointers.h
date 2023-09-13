329149b7f85e2784703d54090638391df835f3ac
xtreme <anastaai94@gmail.com>
2023-09-13 18:47:58 +0100

    modified function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 #endif
