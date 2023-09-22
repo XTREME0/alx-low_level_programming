b7c1362fa7fbc73416a4f0ff862ff3a3a441f3ac
xtreme <anastaai94@gmail.com>
2023-09-22 02:15:09 +0100

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * m_fast_af_boi - function that runs before main
+ * Return: nothing
+ *
+ */
+void __attribute__ ((constructor)) flash()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my" 
+			"house upon my back!\n");
+}
