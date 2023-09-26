ff5623e848f2178f0d45a9b15e8fe7088ef1f17c
xtreme <anastaai94@gmail.com>
2023-09-26 17:47:49 +0100

    first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints all the elements of listint_t
+ *@h: list
+ * Return: number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	int node = 1;
+
+	if (h == NULL)
+		return (0);
+	printf("[%d] ", h->n);
+	if (h->next != NULL)
+	{
+		node++;
+		print_listint(h->next);
+	}
+	return (node);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This dir is for more singly linked lists
