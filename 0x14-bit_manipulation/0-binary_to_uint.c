caf0e71b5d2f7726ebb18a0386e96f7546936183
xtreme <anastaai94@gmail.com>
2023-09-28 20:18:57 +0100

    added 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <math.h>
+/**
+ * binary_to_uint - convert from binary ti decimal
+ *@b: the binary
+ * Return: int
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int num;
+
+	if (b == NULL)
+		return (0);
+
+	for (num = 0; *b != '\0'; b++)
+	{
+		if (*b == '1')
+		{
+			num = num << 1;
+			num++;
+		} else if (*b == '0')
+		{
+			num = num << 1;
+		} else
+		{
+			return (0);
+		}
+	}
+	return (num);
+}
