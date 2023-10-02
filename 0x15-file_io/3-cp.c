f895b4549a0c59e3a085add14aff55b7415f831b
xtreme <anastaai94@gmail.com>
2023-10-02 17:26:19 +0100

    added 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <math.h>
 /**
- * cp - cp file to another file
- *@from: the file to copy from
- *@to: the file to copy to
- * Return: 1 on success -1 if error
- *(const char *from, const char *to)
+ * main - cp file to another file
+ *@ac: number of parameters
+ *@av: files passed as parameter
+ * Return: 0 on success
+ *
  */
 int main(int ac, char **av)
 {
@@ -21,21 +21,18 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	f = open(from, O_RDONLY);
 	if (f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	t = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (t == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
 	while ((b_r = read(f, buffer, sizeof(buffer))) > 0)
 	{
 		b_w = write(t, buffer, b_r);
@@ -45,7 +42,6 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
-			
 	c = close(f);
 	if (c == -1)
 	{
@@ -53,5 +49,4 @@ int main(int ac, char **av)
 		exit(100);
 	}
 	exit(0);
-
 }
