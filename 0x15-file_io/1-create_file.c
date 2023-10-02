ae613b1f937870a196f21e567db99df5173f58b4
xtreme <anastaai94@gmail.com>
2023-10-02 13:51:48 +0100

    added 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,15 +9,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f;
+	int f, e;
 
 	if (filename == NULL)
 		return (-1);
 	f = open(filename, O_WRONLY | O_CREAT, 0600);
 	if (f == -1)
-		return (0);
+		return (-1);
 
-	write(f, text_content, strlen(text_content));
+	e = write(f, text_content, strlen(text_content));
+	if (e == -1)
+		return (-1);
 	close(f);
 	return (1);
 
