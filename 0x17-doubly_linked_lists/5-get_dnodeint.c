6d91a4d23c8ad91b356497100c4dfcc83e086511
xtreme <anastaai94@gmail.com>
2023-11-09 23:11:06 +0100

    added 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get not at index
+ *@head: head
+ *@index: index
+ * Return: nothing
+ *
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *p;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	p = head;
+	while(p && i <= index)
+	{
+		if (i == index)
+			return (p);
+		p = p->next;
+		i++;
+	}
+	return (NULL);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef HEADER
-#define HEADER
+#ifndef HEADER_H
+#define HEADER_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -23,4 +23,5 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 #endif
