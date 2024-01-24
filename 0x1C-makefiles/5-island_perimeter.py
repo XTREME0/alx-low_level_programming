1324c90935026d6d8a5b7560ff7f380c0a19b13a
xtreme <anastaai94@gmail.com>
2024-01-24 17:11:26 +0100

    added 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" island_perimeter function """
+
+def island_perimeter(g):
+    """ island perimeter """
+
+    wdth = len(g[0])
+    ht = len(g)
+    size = 0
+    e = 0
+
+    for i in range(ht):
+        for j in range(wdth):
+            if g[i][j] == 1:
+                size += 1
+                if (i > 0 and g[i - 1][j] == 1):
+                    e += 1
+                if (j > 0 and g[i][j - 1] == 1):
+                    e += 1
+    return size * 4 - e * 2
